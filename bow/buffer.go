d0e8bfc22c0322de5707ce46cccf50835ec01549
nyhu <toussaint.boos@gmail.com>
2019-01-20 00:26:22 +0100

    adding NewBowFromInterfaces wich helps a lot to parse influx results
    adding bow instanciation though row or colums based interfaces
    
    add marshalJSON
    
    add unmarshalJSON and equals method

--- bow/buffer.go
+++ bow/buffer.go
@@ -1,6 +1,114 @@
 package bow
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+const (
+	ErrBufferOverload = "bow: buffer cannot contains all data being sent in channel"
+)
+
 type Buffer struct {
 	Value interface{}
 	Valid []bool
+}
+
+func NewBufferFromInterfaces(t Type, cells []interface{}) (Buffer, error) {
+	return NewBufferFromInterfacesIter(t, len(cells), func() chan interface{}{
+		cellsChan := make(chan interface{})
+		go func () {
+			for _, c := range cells {
+				cellsChan <- c
+			}
+			close(cellsChan)
+		}()
+		return cellsChan
+	}())
+}
+
+func NewBufferFromInterfacesIter(t Type, length int, cells chan interface{}) (Buffer, error) {
+	valid := make([]bool, length)
+	i := 0
+	switch t {
+	case Unknown:
+		return Buffer{}, errors.New("bow: cannot create buffer of unknown type")
+	case Float64:
+		vv := make([]float64, length)
+		for c := range cells {
+			if i >= length {
+				return Buffer{}, errors.New(ErrBufferOverload)
+			}
+			if c != nil {
+				switch c.(type) {
+				case float64:
+					vv[i], valid[i] = c.(float64)
+				case json.Number:
+					f, err := c.(json.Number).Float64()
+					if err != nil {
+						break
+					}
+					vv[i], valid[i] = f, true
+				}
+			}
+			i++
+		}
+		return Buffer{Value:vv, Valid:valid}, nil
+	case Int64:
+		vv := make([]int64, length)
+		for c := range cells {
+			if i >= length {
+				return Buffer{}, errors.New(ErrBufferOverload)
+			}
+			if c != nil {
+				switch c.(type) {
+				case int:
+					tmp, ok := c.(int)
+					vv[i], valid[i] = int64(tmp), ok
+				case json.Number:
+					f, err := c.(json.Number).Int64()
+					if err != nil {
+						break
+					}
+					vv[i], valid[i] = f, true
+				case int64:
+					vv[i], valid[i] = c.(int64)
+				}
+			}
+			i++
+		}
+		return Buffer{Value:vv, Valid:valid}, nil
+	case Bool:
+		vv := make([]bool, length)
+		for c := range cells {
+			if i >= length {
+				return Buffer{}, errors.New(ErrBufferOverload)
+			}
+			if c != nil {
+				vv[i], valid[i] = c.(bool)
+			}
+			i++
+		}
+		return Buffer{Value:vv, Valid:valid}, nil
+	}
+	return Buffer{}, fmt.Errorf("bow: unhandled type number: %v", t)
+}
+
+func seekType(cells []interface{}) (Type, error) {
+	for _, val := range cells {
+		if val != nil {
+			switch val.(type) {
+			case float64, json.Number:
+				return Float64, nil
+			case int, int64:
+				return Int64, nil
+			case string:
+				return Unknown, errors.New("strings are not handled yet")
+			case bool:
+				return Bool, nil
+			}
+		}
+	}
+	return Float64, nil
 }
--- bow/types.go
+++ bow/types.go
@@ -1,11 +1,30 @@
 package bow
 
+import (
+	"fmt"
+	"github.com/apache/arrow/go/arrow"
+)
 
 type Type int
 const (
-	Float64 = iota
+	//Unknown is placed first to be by default
+	// when allocating Type or []Type
+	Unknown = iota
+	Float64
 	Int64
 	Bool
-	//string
+	//string not handled yet
 )
 
+func getTypeFromArrowType(arrowType string) (Type, error) {
+	switch arrowType {
+	case arrow.PrimitiveTypes.Float64.Name():
+		return Float64, nil
+	case arrow.PrimitiveTypes.Int64.Name():
+		return Int64, nil
+	case arrow.FixedWidthTypes.Boolean.Name():
+		return Bool, nil
+	default:
+		return Unknown, fmt.Errorf("bow: unknown type %s", arrowType)
+	}
+}
