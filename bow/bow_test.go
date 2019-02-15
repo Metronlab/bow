2f2ac17e4e9796e57536196e6039fad8261895f9
nyhu <toussaint.boos@gmail.com>
2019-02-15 14:20:43 +0100

    adding failing test and sample with multiple indexes

--- bow/bow_test.go
+++ bow/bow_test.go
@@ -52,6 +52,7 @@ func TestBow_InnerJoin(t *testing.T) {
 
 	bow2, err := NewBow(
 		NewSeries("index", Int64, []int64{1, 2, 3, 5}, nil),
+		NewSeries("col1", Float64, []float64{1.1, 0, 3, 4.4}, []bool{true, false, true, true}),
 		NewSeries("col2", Int64, []int64{1, 2, 3, 4}, nil),
 	)
 	defer bow2.Release()
@@ -60,9 +61,14 @@ func TestBow_InnerJoin(t *testing.T) {
 	}
 
 	expectedBow, err := NewBow(
-		NewSeries("index", Int64, []int64{1, 2, 3}, nil),
-		NewSeries("col1", Float64, []float64{1.1, 2.2, 3.3}, []bool{true, false, true}),
-		NewSeries("col2", Int64, []int64{1, 2, 3}, nil),
+		NewSeries("index", Int64, []int64{
+			1, // present
+			2, // present with col2 value as false on validity bitmap
+			// 3 is absent as there is no matching "col1" index
+			// 4 is absent as there is no matching "index"
+		}, nil),
+		NewSeries("col1", Float64, []float64{1.1, 2.2}, []bool{true, false}),
+		NewSeries("col2", Int64, []int64{1, 2}, nil),
 	)
 	defer expectedBow.Release()
 	if err != nil {
@@ -74,4 +80,22 @@ func TestBow_InnerJoin(t *testing.T) {
 	if !bow3.Equal(expectedBow) {
 		t.Error(expectedBow, bow3)
 	}
+
+	uncomplyingBow, err := NewBow(
+		NewSeries("index", Float64, []float64{1, 2, 3, 4}, nil),
+		NewSeries("col1", Float64, []float64{1.1, 2.2, 3.3, 4.4}, []bool{true, false, true, true}),
+	)
+	defer uncomplyingBow.Release()
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if r := recover(); r == nil ||
+			r.(error).Error() != "bow: left and right bow on join columns are of incompatible types: index" {
+			t.Errorf("indexes of bow1 and uncomplyingBow are uncompatible and should panic. Have %v, expect %v",
+				r, "bow: left and right bow on join columns are of incompatible types: index")
+		}
+	}()
+	bow1.InnerJoin(uncomplyingBow)
 }
