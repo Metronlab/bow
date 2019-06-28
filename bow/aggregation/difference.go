418734eae904bb716fcfe6ddc94871d768e31c60
nyhu <toussaint.boos@gmail.com>
2019-06-28 11:31:50 +0200

    adding difference aggregation

--- bow/aggregation/difference.go
+++ bow/aggregation/difference.go
@@ -1,23 +1,36 @@
 package aggregation
 
-// TODO: to be discussed
-//
-//func Difference(col string) rolling.ColumnAggregation {
-//	first := First(col).Func()
-//	last := Last(col).Func()
-//
-//	// TODO : difference with next windows
-//	return rolling.NewColumnAggregation(col, bow.Float64,
-//		func(col int, w bow.Window) (interface{}, error) {
-//			if w.Bow.NumRows() == 0 {
-//				return nil, nil
-//			}
-//
-//			// TODO: use a getNextVal
-//			value, row := getNextFloat64(w.Bow, col, 0)
-//			if row >= 0 {
-//				return value, nil
-//			}
-//			return nil, nil
-//		})
-//}
+import (
+	"git.prod.metronlab.io/backend_libraries/go-bow/bow"
+	"git.prod.metronlab.io/backend_libraries/go-bow/bow/rolling"
+)
+
+func Difference(col string) rolling.ColumnAggregation {
+	first := First(col).Func()
+	last := Last(col).Func()
+
+	return rolling.NewColumnAggregation(col, true, bow.Float64,
+		func(col int, w bow.Window) (interface{}, error) {
+			if w.Bow.NumRows() == 0 {
+				return nil, nil
+			}
+
+			f, err := first(col, w)
+			if f == nil || err != nil {
+				return nil, err
+			}
+			l, err := last(col, w)
+			if l == nil || err != nil {
+				return nil, err
+			}
+
+			floatedF := bow.Float64.Convert(f)
+			floatedL := bow.Float64.Convert(l)
+
+			if floatedF == nil || floatedL == nil {
+				return nil, nil
+			}
+
+			return floatedL.(float64) - floatedF.(float64), nil
+		})
+}
