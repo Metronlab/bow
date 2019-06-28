641be5919e992e6050ef1b11a2a1f32c7b2398d6
nyhu <toussaint.boos@gmail.com>
2019-06-28 11:31:50 +0200

    adding difference aggregation

--- bow/aggregation/count.go
+++ bow/aggregation/count.go
@@ -8,10 +8,6 @@ import (
 func Count(col string) rolling.ColumnAggregation {
 	return rolling.NewColumnAggregation(col, false, bow.Int64,
 		func(col int, w bow.Window) (interface{}, error) {
-			if w.Bow.NumRows() == 0 {
-				return nil, nil
-			}
-
 			var count int64
 			for i := 0; i < w.Bow.NumRows(); i++ {
 				v := w.Bow.GetValue(col, i)
