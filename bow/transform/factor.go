f61e37ee2f84f9d74101328a815a9e30ace502f7
Romain Baran <romain.baran@metronlab.com>
2019-07-31 10:46:41 +0200

    Accept nil to transform factor

--- bow/transform/factor.go
+++ bow/transform/factor.go
@@ -9,6 +9,8 @@ func Factor(n float64) Transform {
 			return x * n, nil
 		case int64:
 			return int64(float64(x) * n), nil
+		case nil:
+			return x, nil
 		default:
 			return nil, fmt.Errorf("factor: invalid type %T", x)
 		}
