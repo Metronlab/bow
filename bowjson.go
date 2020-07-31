79b6981189d5d30f93b23c164b955125bcce6fbb
agelloz <antoine.gelloz@gmail.com>
2020-07-31 13:11:04 +0200

    Merge master

--- bowjson.go
+++ bowjson.go
@@ -10,15 +10,15 @@ func (b *bow) SetMarshalJSONRowBased(rowOriented bool) {
 }
 
 type jsonColSchema struct {
-	Name string
-	Type string
+	Name string `json:"name"`
+	Type string `json:"type"`
 }
 
 type jsonRecord struct {
 	Schema struct {
-		Columns []jsonColSchema
-	}
-	Rows []map[string]interface{}
+		Fields []jsonColSchema `json:"fields"`
+	} `json:"schema"`
+	Data []map[string]interface{} `json:"data"`
 }
 
 func (b *bow) MarshalJSON() ([]byte, error) {
@@ -28,7 +28,7 @@ func (b *bow) MarshalJSON() ([]byte, error) {
 	}
 	rowBased := jsonRecord{}
 	for _, col := range b.Schema().Fields() {
-		rowBased.Schema.Columns = append(rowBased.Schema.Columns, jsonColSchema{
+		rowBased.Schema.Fields = append(rowBased.Schema.Fields, jsonColSchema{
 			Name: col.Name,
 			Type: col.Type.Name(),
 		})
@@ -37,7 +37,7 @@ func (b *bow) MarshalJSON() ([]byte, error) {
 		if len(row) == 0 {
 			continue
 		}
-		rowBased.Rows = append(rowBased.Rows, row)
+		rowBased.Data = append(rowBased.Data, row)
 	}
 	return json.Marshal(rowBased)
 }
@@ -47,15 +47,15 @@ func (b *bow) UnmarshalJSON(data []byte) error {
 	if err := json.Unmarshal(data, &jsonBow); err != nil {
 		return err
 	}
-	if jsonBow.Rows != nil {
-		series := make([]Series, len(jsonBow.Schema.Columns))
+	if jsonBow.Data != nil {
+		series := make([]Series, len(jsonBow.Schema.Fields))
 		i := 0
-		for _, colSchema := range jsonBow.Schema.Columns {
+		for _, colSchema := range jsonBow.Schema.Fields {
 			t := newTypeFromArrowName(colSchema.Type)
-			buf, err := NewBufferFromInterfacesIter(t, len(jsonBow.Rows), func() chan interface{} {
+			buf, err := NewBufferFromInterfacesIter(t, len(jsonBow.Data), func() chan interface{} {
 				cellsChan := make(chan interface{})
 				go func(cellsChan chan interface{}, colName string) {
-					for _, row := range jsonBow.Rows {
+					for _, row := range jsonBow.Data {
 						val, ok := row[colName]
 						if !ok {
 							cellsChan <- nil
