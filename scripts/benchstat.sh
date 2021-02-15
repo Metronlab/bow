c15340fa56fdf4df91bfeae25141ad4aaf1051dc
agelloz <antoine.gelloz@metronlab.com>
2021-02-15 17:27:54 +0100

    benchmarks on CI

--- scripts/benchstat.sh
+++ scripts/benchstat.sh
@@ -9,13 +9,13 @@ printf "Running benchstat to compare %s and %s\n" "$old_bench_file" "$new_bench_
 if [ ! -f "$old_bench_file" ]
 then
     printf "%s does not exist\n" "$old_bench_file"
-    exit 1
+    exit 0
 fi
 
 if [ ! -f "$new_bench_file" ]
 then
     printf "%s does not exist\n" "$new_bench_file"
-    exit 1
+    exit 0
 fi
 
 benchstat -delta-test none "$old_bench_file" "$new_bench_file"
