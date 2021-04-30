29dc1676749f8a26710a038f2eff776ce1c4a246
agelloz <antoine.gelloz@metronlab.com>
2021-04-30 15:29:43 +0200

    logs

--- rolling/aggregation/weightedmean.go
+++ rolling/aggregation/weightedmean.go
@@ -1,7 +1,6 @@
 package aggregation
 
 import (
-	"fmt"
 	"github.com/metronlab/bow"
 	"github.com/metronlab/bow/rolling"
 	"time"
@@ -17,14 +16,16 @@ func WeightedAverageStep(col string) rolling.ColumnAggregation {
 		func(col int, w rolling.Window) (interface{}, error) {
 			v, err := integralFunc(col, w)
 			if v == nil || err != nil {
-				fmt.Printf("WeightedAverageStep error: %s\n", err)
+				//fmt.Printf("WeightedAverageStep error: %s\n", err)
 				return v, err
 			}
 
 			windowsWide := float64(w.End - w.Start)
-			fmt.Printf("WeightedAverageStep Col:%d\nWindow Bow: Start:%s End:%s isInclusive:%v >> RES:%f/%f=%f\n%+v\n",
-				col, timeFromMillisecond(w.Start).Format(time.RFC3339), timeFromMillisecond(w.End).Format(time.RFC3339), w.IsInclusive,
-				v.(float64), windowsWide, v.(float64)/windowsWide, w.Bow)
+			/*
+				fmt.Printf("WeightedAverageStep Col:%d\nWindow Bow: Start:%s End:%s isInclusive:%v >> RES:%f/%f=%f\n%+v\n",
+					col, timeFromMillisecond(w.Start).Format(time.RFC3339), timeFromMillisecond(w.End).Format(time.RFC3339), w.IsInclusive,
+					v.(float64), windowsWide, v.(float64)/windowsWide, w.Bow)
+			*/
 			return v.(float64) / windowsWide, nil
 		})
 }
@@ -35,14 +36,16 @@ func WeightedAverageLinear(col string) rolling.ColumnAggregation {
 		func(col int, w rolling.Window) (interface{}, error) {
 			v, err := integralFunc(col, w)
 			if v == nil || err != nil {
-				fmt.Printf("WeightedAverageLinear error: %s\n", err)
+				//fmt.Printf("WeightedAverageLinear error: %s\n", err)
 				return v, err
 			}
 
 			windowsWide := float64(w.End - w.Start)
-			fmt.Printf("WeightedAverageLinear Col:%d\nWindow Bow: Start:%s End:%s isInclusive:%v >> RES:%f/%f=%f\n%+v\n",
-				col, timeFromMillisecond(w.Start).Format(time.RFC3339), timeFromMillisecond(w.End).Format(time.RFC3339), w.IsInclusive,
-				v.(float64), windowsWide, v.(float64)/windowsWide, w.Bow)
+			/*
+				fmt.Printf("WeightedAverageLinear Col:%d\nWindow Bow: Start:%s End:%s isInclusive:%v >> RES:%f/%f=%f\n%+v\n",
+					col, timeFromMillisecond(w.Start).Format(time.RFC3339), timeFromMillisecond(w.End).Format(time.RFC3339), w.IsInclusive,
+					v.(float64), windowsWide, v.(float64)/windowsWide, w.Bow)
+			*/
 			return v.(float64) / windowsWide, nil
 		})
 }
