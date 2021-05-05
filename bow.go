f961a7ca172c6a2924eddf124293df5252aa7a72
agelloz <antoine.gelloz@metronlab.com>
2021-05-05 11:26:07 +0200

    logs

--- bow.go
+++ bow.go
@@ -484,18 +484,12 @@ func (b *bow) String() string {
 		return fmt.Sprintf("%s:%v", b.Schema().Field(colIndex).Name, b.GetType(colIndex))
 	})
 
-	fmt.Printf("BOWPRINT rows:%d\n%+v\n", b.NumRows(), b.Schema())
 	// Print each row on buffer
-	rowChan := b.RowMapIter()
+	rowChan := b.FilteredRowMapIter()
 	for row := range rowChan {
 		formatRow(func(colIndex int) string {
 			return fmt.Sprintf("%v", row[b.Schema().Field(colIndex).Name])
 		})
-	}
-
-	_, err := fmt.Fprintf(w, "rows:%d\n", b.NumRows())
-	if err != nil {
-		panic(err)
 	}
 
 	// Flush buffer and format lines along the way
@@ -520,7 +514,6 @@ func (b *bow) rowMapIter(rows chan map[string]interface{}) {
 	}
 
 	for rowIndex := 0; rowIndex < b.NumRows(); rowIndex++ {
-		fmt.Printf("BOWPRINT rows:%d rowIndex:%d\n%+v\n", b.NumRows(), rowIndex, b.Schema())
 		rows <- b.GetRow(rowIndex)
 	}
 }
