bff86502f495e9463e06b63a7eb8aa14d20e5049
agelloz <antoine.gelloz@metronlab.com>
2021-05-07 11:26:02 +0200

    logs

--- rolling/rolling.go
+++ rolling/rolling.go
@@ -116,6 +116,7 @@ func IntervalRollingForIndex(b bow.Bow, colIndex int, interval int64, options Op
 func ValidatePrevRow(b, prevRow bow.Bow) (bow.Bow, error) {
 	if prevRow != nil {
 		if !prevRow.Schema().Equal(b.Schema()) {
+			fmt.Printf("VALIDATEPREVROW ERROR:\nprevRow\n%+v\nb\n%+v\n", prevRow.Schema(), b.Schema())
 			return nil, fmt.Errorf("ValidatePrevRow: b and prevRow must have the same schema")
 		}
 		if prevRow.NumRows() == 0 {
