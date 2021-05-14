7fdaeaacc93095eee13d1f1361122a9e4aea18e7
agelloz <antoine.gelloz@metronlab.com>
2021-05-14 12:28:19 +0200

    revert first last

---
+++ rolling/aggregation/firstlast.go
@@ -0,0 +1,36 @@
+package aggregation
+
+import (
+	"github.com/metronlab/bow"
+	"github.com/metronlab/bow/rolling"
+)
+
+func First(col string) rolling.ColumnAggregation {
+	return rolling.NewColumnAggregation(col, false, bow.InputDependent,
+		func(col int, w rolling.Window) (interface{}, error) {
+			if w.Bow.IsEmpty() {
+				return nil, nil
+			}
+
+			value, irow := w.Bow.GetNextValue(col, 0)
+			if irow == -1 {
+				return nil, nil
+			}
+			return value, nil
+		})
+}
+
+func Last(col string) rolling.ColumnAggregation {
+	return rolling.NewColumnAggregation(col, false, bow.InputDependent,
+		func(col int, w rolling.Window) (interface{}, error) {
+			if w.Bow.IsEmpty() {
+				return nil, nil
+			}
+
+			value, irow := w.Bow.GetPreviousValue(col, w.Bow.NumRows()-1)
+			if irow == -1 {
+				return nil, nil
+			}
+			return value, nil
+		})
+}
