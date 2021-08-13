02d644b486b21733ca76c4b0e8cdc8ff8c055369
agelloz <antoine.gelloz@metronlab.com>
2021-08-13 16:02:47 +0200

    update benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,35 +3,18 @@
 # The -o pipefail option is important for the trap to be executed if the "go test" command fails
 set -o pipefail
 
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+
 : ${PKG:=./...}
 : ${TIMEOUT:="1h"}
+: ${BENCH_RESULTS_DIR_PATH:=/tmp/benchmarks}
+: ${BENCH_RESULTS_FILE_PATH:=/tmp/benchmarks/${TIMESTAMP}.txt}
 
-TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-CURR_BRANCH=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)
+mkdir -p ${BENCH_RESULTS_DIR_PATH}
 
-: ${BENCH_RESULTS_DIR:=/tmp/benchmarks}
-: ${BENCH_RESULTS_CURR:=/tmp/benchmarks/${CURR_BRANCH}.${TIMESTAMP}.txt}
-: ${BENCH_RESULTS_MAIN:=/tmp/benchmarks/main.${TIMESTAMP}.txt}
-
-mkdir -p ${BENCH_RESULTS_DIR}
-
-printf "Running benchmarks to %s\n" "${BENCH_RESULTS_CURR}"
-go test ${PKG} -run=XXX -bench=. -benchmem -timeout ${TIMEOUT} | tee "${BENCH_RESULTS_CURR}"
+printf "Running benchmarks to %s\n" "${BENCH_RESULTS_FILE_PATH}"
+go test ${PKG} -run=XXX -bench=. -benchmem -timeout ${TIMEOUT} | tee "${BENCH_RESULTS_FILE_PATH}"
 
 echo
-printf "Running benchstat on %s\n" "${BENCH_RESULTS_CURR}"
-benchstat "${BENCH_RESULTS_CURR}"
-
-if [ -n "$CI" ]; then
-  echo
-  echo "CI detected - running in comparison mode"
-
-  # compare with main branch
-  git checkout -q -f main
-  echo
-  echo "Running benchmarks on main branch"
-  go test ${PKG} -run=XXX -bench=. -benchmem -timeout ${TIMEOUT} | tee "${BENCH_RESULTS_MAIN}" || echo "Benchmark on main failed"
-
-  git checkout -q -f "$CIRCLE_SHA1"
-  bash ./scripts/benchstat.sh "${BENCH_RESULTS_MAIN}" "${BENCH_RESULTS_CURR}"
-fi
+printf "Running benchstat on %s\n" "${BENCH_RESULTS_FILE_PATH}"
+benchstat "${BENCH_RESULTS_FILE_PATH}"
