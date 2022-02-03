648b33a7485bf7daeba0299565dba8acc181c845
agelloz <antoine.gelloz@metronlab.com>
2022-02-03 14:08:24 +0100

    using the same machine for benchmark comparison

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,20 +4,19 @@
 set -o pipefail
 set -o verbose
 
-BENCH_RESULTS_DIR_PATH=$1
-BENCH_RESULTS_FILE_PATH=$2
-TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+TIMESTAMP="$(date +%Y-%m-%d_%H-%M-%S)"
 
-: ${PKG:=./...}
-: ${TIMEOUT:="1h"}
-: ${RUN:=".*"}
-: ${BENCH_RESULTS_DIR_PATH:="/tmp/benchmarks"}
-: ${BENCH_RESULTS_FILE_PATH:="${BENCH_RESULTS_DIR_PATH}/${TIMESTAMP}.txt"}
+: "${PKG:=./...}"
+: "${TIMEOUT:="1h"}"
+: "${RUN:=".*"}"
+
+BENCH_RESULTS_DIR_PATH=${1:"/tmp/benchmarks"}
+BENCH_RESULTS_FILE_PATH=${2:"${BENCH_RESULTS_DIR_PATH}/${TIMESTAMP}.txt"}
 
 mkdir -p "${BENCH_RESULTS_DIR_PATH}"
 
 printf "Running benchmarks to %s\n" "${BENCH_RESULTS_FILE_PATH}"
-go test ${PKG} -run XXX -bench="${RUN}" -benchmem -timeout ${TIMEOUT} | tee "${BENCH_RESULTS_FILE_PATH}"
+go test ${PKG} -run XXX -bench="${RUN}" -benchmem -timeout "${TIMEOUT}" | tee "${BENCH_RESULTS_FILE_PATH}"
 
 printf "Running benchstat on %s\n" "${BENCH_RESULTS_FILE_PATH}"
 benchstat "${BENCH_RESULTS_FILE_PATH}"
