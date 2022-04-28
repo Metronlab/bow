4335274d1c17ed68d7290ff9fe6576e68926cb7d
agelloz <antoine.gelloz@metronlab.com>
2022-04-28 11:51:03 +0200

    first

--- bowseries_test.go
+++ bowseries_test.go
@@ -3,7 +3,22 @@ package bow
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestNewSeriesFromColBasedInterfaces(t *testing.T) {
+	for _, typ := range allType {
+		t.Run(typ.String(), func(t *testing.T) {
+			testcase := []interface{}{typ.Convert(0), nil}
+			res, err := NewBow(NewSeriesFromInterfaces(typ.String(), typ, testcase))
+			require.NoError(t, err)
+			assert.Equal(t, typ.Convert(0), res.GetValue(0, 0))
+			assert.Equal(t, nil, res.GetValue(0, 1))
+		})
+	}
+}
 
 func BenchmarkNewSeries(b *testing.B) {
 	for rows := 10; rows <= 100000; rows *= 10 {
